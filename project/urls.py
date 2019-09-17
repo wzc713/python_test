376f5d3fab8443bced46cec57c7e4b849efffa73
wzc713 <13460317713@163.com>
2019-09-17 22:44:11 +0800

    123

--- project/urls.py
+++ project/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url,include
-
+from django.views.generic.base import RedirectView
 urlpatterns = [
     #path('admin/', admin.site.urls),
     url(r'admin/',admin.site.urls),
-    url(r'^',include('myApp.urls',namespace='myapp'))
+    url(r'^',include('myApp.urls',namespace='myapp')),
+    url(r'^favicon.ico$',RedirectView.as_view(url=r'static/img/favicon.ico'))
 
 ]
